40a136f12912f669d4ff3f8e24ac7f3313e9da8a
Kevin Faulhaber <kevin.faulhaber@gmail.com>
2022-07-05 23:45:54 +0200

    feat: intitial working version

--- block.ts
+++ block.ts
@@ -1,4 +1,8 @@
-class Block {
+interface Block<T> {
+  index: number;
+  payload: T;
+  hash: string;
+  prevHash: string | null;
 }
 
 export default Block;
--- withHasher.ts
+++ withHasher.ts
@@ -1,7 +1,7 @@
 import hasher from "./hasher.ts";
 import { DigestAlgorithm } from "./typings.ts";
 
-const withHasher = (algorithm: DigestAlgorithm) =>
-  (input: Uint8Array) => hasher(input, algorithm);
+const withHasher = (algorithm: DigestAlgorithm) => (input: Uint8Array) =>
+  hasher(input, algorithm);
 
 export default withHasher;
